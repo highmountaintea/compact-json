5900163902b5b977bebe59cac491f633212e0a2f
highmountaintea <jlpub@coolimage.com>
2019-07-25 16:53:43 -0400

    sort of working

--- client/MainView.js
+++ client/MainView.js
@@ -1,11 +1,69 @@
 const m = require('mithril');
+const parseJson = require('loose-json');
+const stringify = require("json-stringify-pretty-compact");
 
-const model = require('./model');
+// const model = require('./model');
+
+let removeUndefined = true;
+let removeNulls = true;
+let maxLength = '200';
+let input = '';
+let output = '';
+
+function removeNullsFn(obj, remUndef, remNull) {
+  let keys = Object.keys(obj);
+  for (let key of keys) {
+    let val = obj[key];
+    switch (val) {
+      case undefined:
+        if (remUndef) delete obj[key];
+        break;
+      case null:
+        if (remNull) delete obj[key];
+        break;
+      default:
+        if (typeof val === 'object') removeNullsFn(val, remUndef, remNull);
+    }
+  }
+}
+
+function toOutput(inp, remUndef, remNull) {
+  // console.log(inp);
+  let out = parseJson(inp);
+  if (out != null && typeof out === 'object') removeNullsFn(out, remUndef, remNull);
+  let maxlen;
+  try {
+    maxlen = parseInt(maxLength);
+  } catch(e) {
+    maxlen = 80;
+  }
+  let outstr = stringify(out, { maxLength: maxlen, indent: 2 });
+  // console.log(outstr);
+  return outstr;
+}
 
 function render() {
   return m('div', [
-    m('h1', 'Hello World !!'),
-    m('h3', 'Result: ' + JSON.stringify(model.getResult())),
+    m('h3', 'Input:'),
+    m('textarea', { rows: 20, style: 'width: 80%;', value: input, onchange: (ev) => { input = ev.currentTarget.value; } }),
+    m('div',
+      m('input', { type: 'checkbox', checked: removeUndefined, onclick: () => {removeUndefined = !removeUndefined;} }),
+      'Remove undefined',
+    ),
+    m('div',
+      m('input', { type: 'checkbox', checked: removeNulls, onclick: () => {removeNulls = !removeNulls;} }),
+      'Remove nulls',
+    ),
+    m('div',
+      'Max length: ',
+      m('input', { type: 'textbox', value: maxLength, onchange: (ev) => { maxLength = ev.currentTarget.value; } }),
+    ),
+    m('div',
+      m('button', { onclick: () => { output = toOutput(input, removeUndefined, removeNulls); } }, 'Generate Output'),
+    ),
+    m('h3', 'Output:'),
+    m('textarea', { rows: 20, style: 'width: 80%;', value: output, onchange: (ev) => { output = ev.currentTarget.value; } }),
+    // m('h3', 'Result: ' + JSON.stringify(model.getResult())),
   ]);
 }
 
